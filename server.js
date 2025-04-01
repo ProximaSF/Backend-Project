af4df83415e2214eceab86ec5c0eefb1f98d3145
jay walter <proximafactor@gmail.com>
2025-04-01 17:50:20 -0400

    aws 2

--- server.js
+++ server.js
@@ -141,6 +141,7 @@ pool.getConnection().then(database => {
 })
 
 
+const port = process.env.PORT || 3000;
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server running on port ${port}`);
   });
